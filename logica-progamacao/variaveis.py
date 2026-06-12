577664dc1e1bbfe44200ca8dee8e6d7c0651a865
gustavo <gustavovieirapereira99@gmail.com>
2026-06-12 15:01:02 -0300

    12/06/26

---
+++ logica-progamacao/variaveis.py
@@ -0,0 +1,75 @@
+nome = "Giovani"
+idade = 24
+altura = 1.35
+tem_mundial = False 
+
+print(nome)
+print(idade)
+print(altura)
+print(tem_mundial)
+
+
+pessoa = "gustavo"
+time = "corinthans"
+onde_voce_mora =  "predio"
+partido = "direito"
+presidente = "Bolsonaro"
+
+print(pessoa)
+print(time)
+print(moradia)
+print(partido)
+print(presidente)
+
+idade  = 17
+senha = 45
+tamanho_do_pe = 42
+cpf = 1245856951
+telefone = 44998705328
+
+print(idade)
+print(senha)
+print(tamanho_do_pe)
+print(telefone)
+
+
+qual_sua_altura = 1.90
+quanto_voce_pesa = 100
+saldo = 35.500
+horario = 10.50
+quantos_km_voce_corre = 5.6
+
+print(altura)
+print(senha)
+print(saldo)
+print(horario)
+print(quantos_km_voce_corre)
+
+voce_e_bonito = True
+joao_gustavo_e_viado = True
+joao_pedro_foi_corno_para_a_duda = True
+corinthans_maior_time_do_brasil = True
+joao_guilherme_parece_um_macaco = True
+
+print(voce_e_bonito)
+print(joao_gustavo_e_viado)
+print(joao_pedro_foi_corno_para_a_duda)
+print(corinthans_maior_time_do_brasil)
+print(joao_guilherme_parece_um_macaco)
+
+
+
+
+cor = input("digite uma cor:")
+rock = input("digite um musica de rock:")
+
+
+medico = int(input("digite quantas vezes voce vai ao medico por mes:"))
+mercado = int(input("digite quantas vezes voce vai para o mercado por mes"))
+
+
+altura = float(input("qual a sua altura? :"))
+horario = float(input("qual o horario que voce acorda? :"))
+
+aura = input("digite quanto de aura voce tem")
+ego = input("digite quanto de aura voce tem")
