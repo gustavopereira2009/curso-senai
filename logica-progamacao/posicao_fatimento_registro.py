f59a4cf1f5029554158784dd4cd78ec35ee5b6b3
gustavo <gustavovieirapereira99@gmail.com>
2026-09-23 16:48:28 -0300

    23

---
+++ logica-progamacao/posicao_fatimento_registro.py
@@ -0,0 +1,11 @@
+turmas = ["1 Ano A", "1 Ano B", "2 Ano A", "2 Ano B", "3 Ano A", "3 Ano B"]
+posicao = turma.index("2 ano B")
+print(f"A turma '2 ano B' esta na posicao: {posicaio}")
+
+
+
+primeiros_anos = turmas[0:2]
+print("Primeiros anos: "{primeiros_alunos})
+
+terceiros_anos = turmas[-2:]
+print(f"Terceiros alunos: {terceiros_anos}")
